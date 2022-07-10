45bedcd29c3ed94231e625bb58eb885daacd0af4
Jordi Irawan <jordirwn@gmail.com>
2022-07-11 06:39:31 +0700

    create users handler

---
+++ src/handlers/users.handler.js
@@ -0,0 +1,25 @@
+const Users = require('../models/Users');
+const Boom = require('@hapi/boom');
+
+module.exports = {
+  updateUser: async (request, h) => {
+    const userId = request.auth.credentials.userId;
+    const userRes = await Users.updateOne({ _id: userId }, request.payload);
+    return h.response({ updated: Boolean(userRes.modifiedCount) });
+  },
+  getUser: async (request, h) => {
+    const userId = request.auth.credentials.userId;
+    const user = await Users.findById(userId).select('-password -_id');
+    return h.response(user);
+  },
+  deleteUser: async (request, h) => {
+    const { email, password } = request.payload;
+    const userId = request.auth.credentials.userId;
+    const account = await Users.findOne({ _id: userId, email });
+    if (!account || !(await bcrypt.compare(password, account.password))) {
+      return Boom.unauthorized();
+    }
+    const deletedRes = await Users.deleteOne({ _id: userId });
+    return h.response({ deleted: Boolean(deletedRes.deletedCount) });
+  },
+};
